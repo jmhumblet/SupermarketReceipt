6d2a6e0201821c41c76c5a02b9bdd0716185fddc
Thomas Thiry <thomas.thiry@gmail.com>
2021-03-11 15:23:54 +0100

    HandleOffers - Extracted method Discount creation

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -39,58 +39,71 @@ namespace SupermarketReceipt
             foreach (var p in _productQuantities.Keys.Where(offers.ContainsKey))
             {
                 var quantity = _productQuantities[p];
-                var quantityAsInt = (int) quantity;
 
                 var offer = offers[p];
                 var unitPrice = catalog.GetUnitPrice(p);
-                Discount discount = null;
-
-                switch (offer.OfferType)
-                {
-                    case SpecialOfferType.ThreeForTwo:
-                        const int quantityPerOfferThreeForTwo = 3;
-                        
-                        var numberOfXsThreeForTwo = quantityAsInt / quantityPerOfferThreeForTwo;
-                        if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
-                        {
-                            var discountAmount = quantity * unitPrice - (numberOfXsThreeForTwo * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                            discount = new Discount(p, "3 for 2", -discountAmount);
-                        }
-                        break;
-                    
-                    case SpecialOfferType.TwoForAmount:
-                    {
-                        const int quantityPerOfferTwoForAmount = 2;
-                        if (quantityAsInt >= 2)
-                        {
-                            var total = offer.Argument * (quantityAsInt / quantityPerOfferTwoForAmount) + quantityAsInt % 2 * unitPrice;
-                            var discountN = unitPrice * quantity - total;
-                            discount = new Discount(p, "2 for " + offer.Argument, -discountN);
-                        }
-
-                        break;
-                    }
-                    case SpecialOfferType.FiveForAmount:
-                        const int quantityPerOfferFiveForAmount = 5;
-
-                        var numberOfXsFiveForAmount = quantityAsInt / quantityPerOfferFiveForAmount;
-
-
-                        if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
-                        {
-                            var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXsFiveForAmount + quantityAsInt % 5 * unitPrice);
-                            discount = new Discount(p, quantityPerOfferFiveForAmount + " for " + offer.Argument, -discountTotal);
-                        }
-                        break;
-
-                    case SpecialOfferType.TenPercentDiscount:
-                        discount = new Discount(p, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-                        break;
-                }
                 
+                var discount = Discount(offer, quantity, unitPrice, p);
+
                 if (discount != null)
                     receipt.AddDiscount(discount);
             }
+        }
+
+        private static Discount Discount(Offer offer, double quantity, double unitPrice, Product p)
+        {
+            int quantityAsInt = (int)quantity;
+            Discount discount = null;
+
+            switch (offer.OfferType)
+            {
+                case SpecialOfferType.ThreeForTwo:
+                    const int quantityPerOfferThreeForTwo = 3;
+
+                    var numberOfXsThreeForTwo = quantityAsInt / quantityPerOfferThreeForTwo;
+                    if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+                    {
+                        var discountAmount = quantity * unitPrice -
+                                             (numberOfXsThreeForTwo * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                        discount = new Discount(p, "3 for 2", -discountAmount);
+                    }
+
+                    break;
+
+                case SpecialOfferType.TwoForAmount:
+                {
+                    const int quantityPerOfferTwoForAmount = 2;
+                    if (quantityAsInt >= 2)
+                    {
+                        var total = offer.Argument * (quantityAsInt / quantityPerOfferTwoForAmount) +
+                                    quantityAsInt % 2 * unitPrice;
+                        var discountN = unitPrice * quantity - total;
+                        discount = new Discount(p, "2 for " + offer.Argument, -discountN);
+                    }
+
+                    break;
+                }
+                case SpecialOfferType.FiveForAmount:
+                    const int quantityPerOfferFiveForAmount = 5;
+
+                    var numberOfXsFiveForAmount = quantityAsInt / quantityPerOfferFiveForAmount;
+
+
+                    if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
+                    {
+                        var discountTotal = unitPrice * quantity -
+                                            (offer.Argument * numberOfXsFiveForAmount + quantityAsInt % 5 * unitPrice);
+                        discount = new Discount(p, quantityPerOfferFiveForAmount + " for " + offer.Argument, -discountTotal);
+                    }
+
+                    break;
+
+                case SpecialOfferType.TenPercentDiscount:
+                    discount = new Discount(p, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
+                    break;
+            }
+
+            return discount;
         }
     }
 }
