e5f8626a7426382e48d783e9906fae52d14bbc0b
Thomas Thiry <thomas.thiry@gmail.com>
2021-03-11 15:08:47 +0100

    HandleOffers - Move ThreeForTwo code to switch

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -44,35 +44,45 @@ namespace SupermarketReceipt
                 var offer = offers[p];
                 var unitPrice = catalog.GetUnitPrice(p);
                 Discount discount = null;
-                var x = 1;
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo)
+                var quantityPerOffer = 1;
+                switch (offer.OfferType)
                 {
-                    x = 3;
-                }
-                else if (offer.OfferType == SpecialOfferType.TwoForAmount)
-                {
-                    x = 2;
-                    if (quantityAsInt >= 2)
+                    case SpecialOfferType.ThreeForTwo:
+                        quantityPerOffer = 3;
+                        
+                        var numberOfXsThreeForTwo = quantityAsInt / quantityPerOffer;
+                        if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+                        {
+                            var discountAmount = quantity * unitPrice - (numberOfXsThreeForTwo * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                            discount = new Discount(p, "3 for 2", -discountAmount);
+                        }
+                        break;
+                    
+                    case SpecialOfferType.TwoForAmount:
                     {
-                        var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                        var discountN = unitPrice * quantity - total;
-                        discount = new Discount(p, "2 for " + offer.Argument, -discountN);
+                        quantityPerOffer = 2;
+                        if (quantityAsInt >= 2)
+                        {
+                            var total = offer.Argument * (quantityAsInt / quantityPerOffer) + quantityAsInt % 2 * unitPrice;
+                            var discountN = unitPrice * quantity - total;
+                            discount = new Discount(p, "2 for " + offer.Argument, -discountN);
+                        }
+
+                        break;
                     }
+                    case SpecialOfferType.FiveForAmount:
+                        quantityPerOffer = 5;
+                        break;
                 }
 
-                if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
-                var numberOfXs = quantityAsInt / x;
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
-                {
-                    var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(p, "3 for 2", -discountAmount);
-                }
+                var numberOfXs = quantityAsInt / quantityPerOffer;
+
 
                 if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(p, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
                 if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
                 {
                     var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(p, x + " for " + offer.Argument, -discountTotal);
+                    discount = new Discount(p, quantityPerOffer + " for " + offer.Argument, -discountTotal);
                 }
 
                 if (discount != null)
