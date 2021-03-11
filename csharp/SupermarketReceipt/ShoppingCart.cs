edaf88221d980f0093cba0d3423c3a56321f28d2
Thomas Thiry <thomas.thiry@gmail.com>
2021-03-11 15:58:21 +0100

    Offer - Make DefineDiscount non static

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -44,89 +44,11 @@ namespace SupermarketReceipt
                 var offer = offers[p];
                 var unitPrice = catalog.GetUnitPrice(p);
                 
-                var discount = DefineDiscount(offer, quantity, unitPrice, p);
+                var discount = offer.DefineDiscount(quantity, unitPrice, p);
 
                 if (discount != null)
                     receipt.AddDiscount(discount);
             }
-        }
-
-        private static Discount DefineDiscount(Offer offer, double quantity, double unitPrice, Product product)
-        {
-            switch (offer.OfferType)
-            {
-                case SpecialOfferType.ThreeForTwo:
-                {
-                    return DefineThreeForTwoDiscount(offer, quantity, unitPrice, product);
-                }
-                case SpecialOfferType.TwoForAmount:
-                {
-                    return DefineTwoForAmountDiscount(offer, quantity, unitPrice, product);
-                }
-                case SpecialOfferType.FiveForAmount:
-                {
-                    return DefineFiveForAmountDiscount(offer, quantity, unitPrice, product);
-                }
-                case SpecialOfferType.TenPercentDiscount:
-                {
-                    return DefineTenPercentDiscount(offer, quantity, unitPrice, product);
-                }
-                default:
-                {
-                    return null;
-                }
-            }
-        }
-
-        private static Discount DefineTenPercentDiscount(Offer offer, double quantity, double unitPrice, Product product)
-        {
-            return new Discount(product, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-        }
-
-        private static Discount DefineFiveForAmountDiscount(Offer offer, double quantity, double unitPrice, Product product)
-        {
-            const int quantityPerOfferFiveForAmount = 5;
-
-            var numberOfXsFiveForAmount = (int) quantity / quantityPerOfferFiveForAmount;
-
-
-            if (offer.OfferType == SpecialOfferType.FiveForAmount && (int) quantity >= 5)
-            {
-                var discountTotal = unitPrice * quantity -
-                                    (offer.Argument * numberOfXsFiveForAmount + (int) quantity % 5 * unitPrice);
-                return new Discount(product, quantityPerOfferFiveForAmount + " for " + offer.Argument, -discountTotal);
-            }
-
-            return null;
-        }
-
-        private static Discount DefineTwoForAmountDiscount(Offer offer, double quantity, double unitPrice, Product product)
-        {
-            const int quantityPerOfferTwoForAmount = 2;
-            if ((int) quantity >= 2)
-            {
-                var total = offer.Argument * ((int) quantity / quantityPerOfferTwoForAmount) +
-                            (int) quantity % 2 * unitPrice;
-                var discountN = unitPrice * quantity - total;
-                return new Discount(product, "2 for " + offer.Argument, -discountN);
-            }
-
-            return null;
-        }
-
-        private static Discount DefineThreeForTwoDiscount(Offer offer, double quantity, double unitPrice, Product product)
-        {
-            const int quantityPerOfferThreeForTwo = 3;
-
-            var numberOfXsThreeForTwo = (int) quantity / quantityPerOfferThreeForTwo;
-            if (offer.OfferType == SpecialOfferType.ThreeForTwo && (int) quantity > 2)
-            {
-                var discountAmount = quantity * unitPrice -
-                                     (numberOfXsThreeForTwo * 2 * unitPrice + (int) quantity % 3 * unitPrice);
-                return new Discount(product, "3 for 2", -discountAmount);
-            }
-
-            return null;
         }
     }
 }
